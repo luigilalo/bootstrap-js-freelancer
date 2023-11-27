62ca51e9178106ca178b0745420abcd4869d4db1
luigilalo <luigilaloe1@gmail.com>
2023-11-27 10:51:35 +0100

    Second Commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Dichiarazione Array per i Codici Sconto
+let discountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+
+// Valore orario del lavoro
+const backEnd = 20.50;
+const frontEnd = 15.30;
+const dataAnalyst = 33.60;
+
+// Dati dal form 
+function formSubmitted(event){
+    event.preventDefault();
+
+
+
+}
