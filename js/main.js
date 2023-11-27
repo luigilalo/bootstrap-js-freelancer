e1e117937492c24d16390fde86994d92cb30c66e
luigilalo <luigilaloe1@gmail.com>
2023-11-27 12:24:41 +0100

    Discount working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Dichiarazione Array per i Codici Sconto
-let discountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+let listOfDiscountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
 
 // Valore orario del lavoro
 const backEnd = 20.50;
@@ -9,6 +9,9 @@ const dataAnalyst = 33.60;
 // Dati dal form 
 function formSubmitted(event) {
     event.preventDefault();
+
+    let totalPrice = 0
+    let promo = 0 
 
     // NOME
      let inputName= document.getElementById("FormFirstName").value;
@@ -29,4 +32,31 @@ function formSubmitted(event) {
      // LAVORO RICHIESTO
      let typeOfWork = document.getElementById("ChoosenWork").value;
      console.log(typeOfWork);
+
+     // CODICE SCONTO
+     let discountCode = document.getElementById("FormDiscountCode").value;
+     console.log(discountCode);
+
+     // LA SCELTA TRA I TRE LAVORI
+     if (typeOfWork == "backend") {
+     totalPrice = backEnd * hoursOfWork
+     console.log(totalPrice.toFixed(2))
+     }
+     else if (typeOfWork == "frontend") {
+     totalPrice = frontEnd * hoursOfWork 
+     console.log(totalPrice.toFixed(2))
+    }
+     else {totalPrice = dataAnalyst * hoursOfWork
+           console.log(totalPrice.toFixed(2))
+    }
+
+    // CONTROLLO SE IL DISCOUNT CODE INSERITO E' PRESENTE NELL'ARRAY
+    for (let i = 0; i < listOfDiscountCodes.length; i++) {
+        if (listOfDiscountCodes[i] == discountCode) {
+            promo = totalPrice * 0.25;
+            totalPrice = totalPrice - promo;
+            console.log("CODICE SCONTO VALIDO");
+            console.log("Il prezzo scontato è:" + " " + totalPrice.toFixed(2))
+        }
+}
 }
